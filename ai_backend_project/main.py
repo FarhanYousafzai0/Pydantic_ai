3a2dd0b55429d0e5c7d7af07812d3ff958139f83
Farhan Yousafzai <coderwithferry@gmail.com>
2026-02-18 17:08:51 +0500

    Starting working on the Backend

--- ai_backend_project/main.py
+++ ai_backend_project/main.py
@@ -1,41 +1,96 @@
-from fastapi import FastAPI
+
+
+
+
+
+
+
+
+from fastapi import FastAPI,HTTPException
 from pydantic import BaseModel
-import logging 
+from logging.handlers import RotatingFileHandler
+import logging
+from dotenv import load_dotenv
+import os
+
+
+# Log Dotenv File :
+
+load_dotenv();
+
+APP_NAME = os.getenv("APP_NAME","Default App Name");
+LOG_LEVEL = os.getenv("LOG_LEVEL","INFO").upper();
+DEBUG = os.getenv("DEBUG","true") == "true";
+
+
+os.makedirs("logs", exist_ok=True)
+
+logger = logging.getLogger("app_logger")
+logger.setLevel(getattr(logging,LOG_LEVEL,logging.INFO))
+
+formatter = logging.Formatter(
+    "%(asctime)s - %(name)s - %(levelname)s - %(message)s"
+)
+
+console_handler = logging.StreamHandler()
+console_handler.setFormatter(formatter)
+
+file_handler = RotatingFileHandler(
+    "logs/app.log",
+    maxBytes=1_000_000,
+    backupCount=3
+)
+file_handler.setFormatter(formatter)
+
+logger.addHandler(console_handler)
+logger.addHandler(file_handler)
 
 
 
-logging.basicConfig(level=logging.INFO,format="%(asctime)s - %(name)s - %(levelname)s - %(message)s")
-
-
-logger = logging.getLogger(__name__)
-app = FastAPI()
-
-
-
+app = FastAPI();
 
 class User(BaseModel):
     name:str
     age:int
 
+
+
 @app.get("/")
 def root():
     logger.info("Root endpoint called")
-    return {"message": "API is running"}
+    return {
+        "app": APP_NAME,
+        "debug": DEBUG,
+        "message": "API is running"
+    }
+
+    
+
+
 
 
 
 @app.post("/user")
+
 def create_user(user:User):
+    try:
+        logger.debug(f"Received raw user data: {user.model_dump_json()}")
+        logger.info(f"Creating user: {user.name} with age {user.age}")
 
-    logger.info(f"Creating user {user.name} with age {user.age}")
-    result = {
-        "message":f"User {user.name} created successfully",
-        "age_in_5_years":f"User age will be in next 5 years will be {user.age + 5}"
-    }
-
-    logger.info(f"User {user.name} created successfully")
-    return result
+        if user.age < 0:
+            logger.warning(f"Age cannot be negative for user: {user.name}")
+            raise HTTPException(status_code=400, detail="Age cannot be negative")
 
 
+        result = {
+            "message":f"User {user.name} created successfully",
+            "age_in_5_years":user.age + 5
+        }
+        logger.debug(f"Returning result: {result}")
+        logger.info(f"User {user.name} created successfully")
+        return result
 
+    except Exception as e:
+        logger.error(f"Error creating user: {e}")
+        raise HTTPException(status_code=500, detail=str(e))
 
